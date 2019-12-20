ffa83496f0f62e32954dc2f36b5b788ae315e919
faith nenkai <nenkaifaith1@gmail.com>
2019-12-20 09:44:19 +0300

    Modify html

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-document.forms['form'].addEventListener('submit', function (event) {
-  event.preventDefault();
-});
 function checkBlank() {
   var name = document.forms['form'].elements[0].value;
   var email = document.forms['form'].elements[1].value;
@@ -27,7 +24,7 @@ function _p(a) { return a.preventDefault(); }
 var globVarA;
 _i("close").addEventListener('click', function () { this.removeEventListener('click', choose); _i('details').style.display = "none"; });
 // _i("closeb").addEventListener('click', function () { this.removeEventListener('click', choose); _i('cart').style.display = "none"; });
- _i("buyBtn").addEventListener('click', function () { this.removeEventListener('click', choose);});
+_i("buyBtn").addEventListener('click', function () { this.removeEventListener('click', choose); });
 
 _f('details').addEventListener('submit', function (e) { _p(e); })
 function btnCust() {
@@ -37,82 +34,108 @@ function btnCust() {
     a[c].addEventListener('click', choose);
   }
 } btnCust();
+
+$(document).ready(function () {
+  $('form[name="details"]').submit(function (event) {
+    var quantity = parseInt($("#quantity").val());
+    var type = parseInt($("#type").val());
+    var crust = parseInt($("#crust").val());
+    var toppings = parseInt($("#toppings").val());
+    var moreToppings = parseInt($("#moreToppings").val());
+
+    var pizzaOrder = new PizzaOrder(quantity, type, crust, toppings, moreToppings)
+
+    $("#detailsPrice").html(pizzaOrder.orderPrice())
+    event.preventDefault()
+  })
+})
+function PizzaOrder(quantity, pizzaType, pizzaCrust, pizzaToppings, moreToppings) {
+  this.quantity = quantity;
+  this.type = pizzaType;
+  this.crust = pizzaCrust;
+  this.toppings = pizzaToppings;
+  this.moreToppings = moreToppings;
+}
+PizzaOrder.prototype.orderPrice = function () {
+  var total = this.quantity * (this.type + this.crust + this.toppings + this.moreToppings)
+  return total;
+}
 function choose() {
   var a, b, c, d;
   a = this.parentNode.children;
   _i('details').style.display = "block";
   _i('detailsName').innerText = a[1].innerText;
   _i('detailsImg').src = a[0].src;
-  d = a[2].innerText
-  _i('detailsPrice').innerText = parseInt(d);
-  globVarA=parseInt(d);
-  _f('details')[0].value = 1;
-  for (c = 0; c < _f('details').length; c++) { _f('details')[c].addEventListener('change', detailChange); }
+  // d = a[2].innerText
+  // _i('detailsPrice').innerText = parseInt(d);
+  // globVarA = parseInt(d);
+  // _f('details')[0].value = 1;
+  // for (c = 0; c < _f('details').length; c++) { _f('details')[c].addEventListener('change', detailChange); }
 }
-function detailChange() {
-  var a, b, c, d, e, f, g;
-  a = parseFloat(qnty()); b = parseFloat(ram()); c = parseFloat(storage()); d = parseFloat(cpu()); e = parseFloat(display());
-  _i('detailsPrice').innerText = a * (globVarA + b + c + d + e);
-  f = _i('detailsPrice').innerText = a * (globVarA + b + c + d + e);
-  _i('buyBtn').addEventListener('click', function () {
-    alert("Grand Tota:Ksh " + f);
-    if (true) {
-      confirm("Do you want it delivered? Delivery fee = Ksh.50");
-      prompt('PLease enter your location');
-      alert('Your delivery will be made soon.Thank you for using our services.');
-      return 0;
-    }
+// function detailChange() {
+//   var a, b, c, d, e, f, g;
+//   a = parseFloat(qnty()); b = parseFloat(ram()); c = parseFloat(storage()); d = parseFloat(cpu()); e = parseFloat(display());
+//   _i('detailsPrice').innerText = a * (globVarA + b + c + d + e);
+//   f = _i('detailsPrice').innerText = a * (globVarA + b + c + d + e);
+//   _i('buyBtn').addEventListener('click', function () {
+//     alert("Grand Tota:Ksh " + f);
+//     if (true) {
+//       confirm("Do you want it delivered? Delivery fee = Ksh.50");
+//       prompt('Please enter your location');
+//       alert('Your delivery will be made soon.Thank you for using our services.');
+//       return 0;
+//     }
 
-  });
-}
+//   });
+// }
 
 
-function qnty() {
-  var a = parseInt(_f('details')[0].value);
-  if (a < 1) { _f('details')[0].value = 1; a = 1; }
-  return a;
-}
+// function qnty() {
+//   var a = parseInt(_f('details')[0].value);
+//   if (a < 1) { _f('details')[0].value = 1; a = 1; }
+//   return a;
+// }
 
-function ram() {
-  switch (parseInt(_f('details')[1].value)) {
-    case 1: return 0; break;
-    case 2: return 2 * globVarA * 0.05; break;
-    case 3: return 4 * globVarA * 0.05; break;
-    case 4: return 8 * globVarA * 0.05; break;
-    case 5: return 16 * globVarA * 0.05; break;
-    default: alert(_f('details')[1].value + 'invalid Selection Ram'); break;
-  }
-}
-function storage() {
-  switch (parseInt(_f('details')[2].value)) {
-    case 1: return 0; break;
-    case 2: return 2 * globVarA * 0.1; break;
-    case 3: return 4 * globVarA * 0.1; break;
-    case 4: return 8 * globVarA * 0.1; break;
-    case 5: return 16 * globVarA * 0.1; break;
-    default: alert(_f('details')[2].value + 'invalid Selection Storage'); break;
-  }
-}
-function cpu() {
-  switch (parseInt(_f('details')[3].value)) {
-    case 1: return 0; break;
-    case 2: return 2 * globVarA * 0.2; break;
-    case 3: return 4 * globVarA * 0.2; break;
-    case 4: return 8 * globVarA * 0.2; break;
-    case 5: return 16 * globVarA * 0.2; break;
-    default: alert(_f('details')[3].value + 'invalid Selection cpu'); break;
-  }
-}
-function display() {
-  switch (parseInt(_f('details')[4].value)) {
-    case 1: return 0; break;
-    case 2: return 1 * globVarA * -0.1; break;
-    case 3: return 2 * globVarA * 0.2; break;
-    case 4: return 2 * globVarA * 0.1; break;
-    case 5: return 4 * globVarA * 0.4; break;
-    case 6: return 4 * globVarA * 0.3; break;
-    default: alert(_f('details')[4].value + 'invalid Selection display'); break;
-  }
-}
+// function ram() {
+//   switch (parseInt(_f('details')[1].value)) {
+//     case 1: return 0; break;
+//     case 2: return 2 * globVarA * 0.05; break;
+//     case 3: return 4 * globVarA * 0.05; break;
+//     case 4: return 8 * globVarA * 0.05; break;
+//     case 5: return 16 * globVarA * 0.05; break;
+//     default: alert(_f('details')[1].value + 'invalid Selection Ram'); break;
+//   }
+// }
+// function storage() {
+//   switch (parseInt(_f('details')[2].value)) {
+//     case 1: return 0; break;
+//     case 2: return 2 * globVarA * 0.1; break;
+//     case 3: return 4 * globVarA * 0.1; break;
+//     case 4: return 8 * globVarA * 0.1; break;
+//     case 5: return 16 * globVarA * 0.1; break;
+//     default: alert(_f('details')[2].value + 'invalid Selection Storage'); break;
+//   }
+// }
+// function cpu() {
+//   switch (parseInt(_f('details')[3].value)) {
+//     case 1: return 0; break;
+//     case 2: return 2 * globVarA * 0.2; break;
+//     case 3: return 4 * globVarA * 0.2; break;
+//     case 4: return 8 * globVarA * 0.2; break;
+//     case 5: return 16 * globVarA * 0.2; break;
+//     default: alert(_f('details')[3].value + 'invalid Selection cpu'); break;
+//   }
+// }
+// function display() {
+//   switch (parseInt(_f('details')[4].value)) {
+//     case 1: return 0; break;
+//     case 2: return 1 * globVarA * -0.1; break;
+//     case 3: return 2 * globVarA * 0.2; break;
+//     case 4: return 2 * globVarA * 0.1; break;
+//     case 5: return 4 * globVarA * 0.4; break;
+//     case 6: return 4 * globVarA * 0.3; break;
+//     default: alert(_f('details')[4].value + 'invalid Selection display'); break;
+//   }
+// }
 
 
